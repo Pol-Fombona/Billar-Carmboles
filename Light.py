e842ba7c419b8a424f62e858b98faa275f63ee50
tonborrell <tonborrell@gmail.com>
2022-11-27 21:18:26 +0100

    delete unused variable

--- Light.py
+++ Light.py
@@ -5,7 +5,7 @@ from positions import LIGHT_POSITION
 
 class Light():
 
-    def __init__(self, position=LIGHT_POSITION, position2=(0, 5, 0), color=(1,1,1)):
+    def __init__(self, position=LIGHT_POSITION, color=(1,1,1)):
         self.position = glm.vec3(position)
         # self.position2 = glm.vec3(position2)
         self.color = glm.vec3(color)
