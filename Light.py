6e89a64649dd79981d257df7ae7e0e3bb083e4c5
tonborrell <tonborrell@gmail.com>
2022-12-01 13:55:20 +0100

    small change

--- Light.py
+++ Light.py
@@ -2,7 +2,7 @@
 import glm
 class Light():
 
-    def __init__(self, position=(0, 0, 0), Ia = 0.1, Id = 0.8, Is = 1.0, color=(1,1,1)):
+    def __init__(self, position, Ia = 0.1, Id = 0.8, Is = 1.0, color=(1,1,1)):
         self.position = glm.vec3(position)
         # self.position2 = glm.vec3(position2)
         self.color = glm.vec3(color)
