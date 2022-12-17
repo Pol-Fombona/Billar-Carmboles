e08aea81723df811ac56cad339e3cb70eb4edc2e
tonborrell <tonborrell@gmail.com>
2022-12-17 19:11:17 +0100

    fix sostre and change normals

--- shaders/terra.frag
+++ shaders/terra.frag
@@ -33,8 +33,8 @@ vec3 getLight(vec3 color){
     float diff2 = max(0, dot(lightDir2, Normal));
     float diff3 = max(0, dot(lightDir3, Normal));
     vec3 diffuse = diff * light.Id;
-    vec3 diffuse2 = diff2 * light2.Id;
-    vec3 diffuse3 = diff3 * light3.Id;
+    vec3 diffuse2 = diff2 * light2.Id*0.55;
+    vec3 diffuse3 = diff3 * light3.Id*0.55;
 
     // specular
     vec3 viewDir = normalize(camPos - fragPos);
@@ -44,9 +44,9 @@ vec3 getLight(vec3 color){
     float spec = pow(max(dot(viewDir, reflectDir), 0), 12);
     float spec2 = pow(max(dot(viewDir, reflectDir2), 0), 12);
     float spec3 = pow(max(dot(viewDir, reflectDir3), 0), 12);
-    vec3 specular = spec * light.Is * 2;
-    vec3 specular2 = spec2 * light2.Is * 2;
-    vec3 specular3 = spec3 * light3.Is * 2;
+    vec3 specular = spec * light.Is * 0.8;
+    vec3 specular2 = spec2 * light2.Is * 0.8;
+    vec3 specular3 = spec3 * light3.Is * 0.8;
 
     return color * (ambient + diffuse + specular + ambient2 + diffuse2 + specular2 + ambient3 + diffuse3 + specular3);
 }
--- vbo.py
+++ vbo.py
@@ -703,7 +703,7 @@ class TerraVBO(BaseVBO):
         tex_coord_data = self.get_data(tex_coord, tex_coord_indices)
 
         normals = [
-            (0, 0, 1) * 6
+            (0, 1, 0) * 6
         ]
 
         normals = np.array(normals, dtype="f4").reshape(6, 3)
@@ -726,21 +726,20 @@ class SostreVBO(BaseVBO):
         return np.array(data, dtype='f4')
 
     def get_vertex_data(self): #sobre eix y
-        
-        vertices = [(-150,S_HEIGHT,-150),(S_X,S_HEIGHT,S_Z),(-150,S_HEIGHT,S_Z),(S_X,S_HEIGHT,-150)]
-        indices = [(0,1,3),(0,2,1)]
-        
-
+        vertices = [(INICI_TERRA, S_HEIGHT, INICI_TERRA), (T_X, S_HEIGHT, INICI_TERRA), (T_X, S_HEIGHT, T_Z), (INICI_TERRA, S_HEIGHT, T_Z)]
+        indices = [(0, 1, 2), (0, 2, 3)]
         
         vertex_data = self.get_data(vertices, indices)
         
-        tex_coord = [(0,0), (1,0), (1,1), (0,1)]
-        
-        tex_coord_indices = [(0,1,3),(0,2,1)]
+        tex_coord = [(0, 0), (1, 0), (1, 1), (0, 1), (0.8, 0), (0.8, 0.8), (0, 0.8)]
+        tex_coord_indices = [
+            (0, 1, 2),
+            (0, 2, 3),
+        ]
         tex_coord_data = self.get_data(tex_coord, tex_coord_indices)
 
         normals = [
-            (0, 0, -1) * 6,
+            (0, -1, 0) * 6
         ]
         
         normals = np.array(normals, dtype="f4").reshape(6, 3)
